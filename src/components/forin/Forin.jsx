64397c0a0d0bd00cc6053ec78508d0c1a8aaa9bd
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-27 20:57:54 +0500

    add input

--- src/components/forin/Forin.jsx
+++ src/components/forin/Forin.jsx
@@ -1,6 +1,28 @@
 const Forin = () => {
   return (
-    <div>Forin</div>
+    <div>
+      <div className="forin">
+        <div className="container">
+          <div className="forin_box">
+            <form  className="forin_card">
+              <input  className="forin_input" 
+                type="text"   
+                placeholder="Username"  
+                required 
+              />
+              <input className="forin_input"
+                type="password" 
+                placeholder="Password" 
+                required 
+              />
+              <button className="forin_btn" type="submit" >
+                        {"login"}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
